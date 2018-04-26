469fb609352da8e2fb9aac42b1aeece3e40d0b32
Mateusz Chojnacki <mateuszchojnacki@MacBook-Pro-Mateusz.local>
2018-04-26 23:39:57 +0200

    try hard like all photos on wall

--- script.py
+++ script.py
@@ -1,5 +1,12 @@
 import config
 from selenium import webdriver
+import time
+#
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+#
 
 driver = webdriver.Chrome("/Users/mateuszchojnacki/Applications/chromedriver")
 def login_twitter(username, password):
@@ -26,19 +33,36 @@ def openHashtag(hashtag):
     inputElement = driver.find_element_by_id("search-query")
     inputElement.send_keys(hashtag)
     driver.find_element_by_class_name("nav-search").click()
-    # wait here
 
-    driver.implicitly_wait(120)
+
+def likeAll():
     manyElements = driver.find_elements_by_class_name("HeartAnimation")
+    print("LIKE! before \n")
     for element in manyElements:
-        element.click()
-    driver.implicitly_wait(10)
-
+        print("LIKE! inside \n")
+        try:
+            iserror = element.click()
+        finally:
+            element.click()
+    print("LIKE! after \n")
 
 if __name__ == "__main__":
     username = config.DATACOUP_USERNAME
     password = config.DATACOUP_PASSWORD
     twitt = "Test tweet"
     login_twitter(username, password)
-    # sendTwitt(twitt)
-    openHashtag("#F4F")
+    # openHashtag("#ehelons")
+    # try:
+    #     element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "AdaptiveFiltersBar-item")))
+    # finally:
+    #     likeAll()
+    likeAll()
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     # likeAll()
+    #
+    # while True:
+    #     print("5secound -> scrool -> like")
+    #     time.sleep(5)   # Delay for 1 minute (60 seconds).
+    #     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #     likeAll()
+    #     time.sleep(20)
